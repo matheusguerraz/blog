5afbdb47d2002015187f7adc0422db3f4bd06cf3
Matheus de Mello Guerra <matheusguerra174@gmail.com>
2023-06-07 00:31:58 -0300

    template about me and styles

--- scripts.js
+++ scripts.js
@@ -1,60 +1,48 @@
-const texts = {
-  introduct: "Hi, my name is",
-  name: "Matheus Guerra.",
-  resume: "I'm a Python Backend Developer",
-  description: "I have skills in web development with Django, experience with task automation and data scraping."
-};
 
-function typeWriter(element, text, delay = 60) {
-  let charIndex = 0;
 
-  function type() {
-    if (charIndex < text.length) {
-      element.textContent += text.charAt(charIndex);
-      charIndex++;
-      setTimeout(type, delay);
-    }
+document.addEventListener('DOMContentLoaded', function() {
+  const container = document.querySelector('.container');
+
+  if (container && container.innerHTML.trim() === '') {
+    // O container está vazio, preenche com as informações da constante texts
+    const elements = Object.keys(texts).map(function(key) {
+      return `<p>${texts[key]}</p>`;
+    });
+
+    container.innerHTML = elements.join('');
   }
 
-  type();
-}
+  // Event listener para o link "Home"
+  const homeLink = document.getElementById('homeLink');
+  homeLink.addEventListener('click', function(event) {
+    event.preventDefault(); // Impede o comportamento padrão do link
+    openTab('index.html'); // Chama a função openTab com o parâmetro 'index'
+  });
 
-const elements = document.querySelectorAll("#centered-text p");
-const ids = Object.keys(texts);
+  // Event listener para o link "About"
+  const aboutLink = document.getElementById('aboutLink');
+  aboutLink.addEventListener('click', function(event) {
+    event.preventDefault(); // Impede o comportamento padrão do link
+    openTab('about'); // Chama a função openTab com o parâmetro 'about'
+  });
 
-function writeLines(index) {
-  if (index < elements.length) {
-    const element = elements[index];
-    const text = texts[ids[index]];
-    typeWriter(element, text, 100);
-    setTimeout(() => writeLines(index + 1), text.length * 100  + 100);
-  }
-}
-
-writeLines(0);  
+  // Event listener para o link "Projects"
+  const projectsLink = document.getElementById('projectsLink');
+  projectsLink.addEventListener('click', function(event) {
+    event.preventDefault(); // Impede o comportamento padrão do link
+    openTab('Projects'); // Chama a função openTab com o parâmetro 'Projects'
+  });
+});
 
 function openTab(tabName) {
-  // Cria uma nova requisição AJAX
   const xhr = new XMLHttpRequest();
-
-  // Define o callback a ser chamado quando a requisição estiver concluída
   xhr.onload = function() {
     if (xhr.status === 200) {
-      // Remove o conteúdo atual do <body>
-      document.body.innerHTML = '';
-
-      // Insere o conteúdo do arquivo no <body>
       document.body.innerHTML = xhr.responseText;
     } else {
       console.error('Erro ao carregar o arquivo:', xhr.status);
     }
   };
-
-  // Define o método e o URL da requisição
   xhr.open('GET', `${tabName}.html`, true);
-
-  // Envia a requisição
   xhr.send();
 }
-
-
