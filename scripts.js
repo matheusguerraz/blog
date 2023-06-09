f0ef1b76d786531ca3e809030d7f761b45eb2fef
Matheus de Mello Guerra <matheusguerra174@gmail.com>
2023-06-09 15:18:38 -0300

    initial project template

--- scripts.js
+++ scripts.js
@@ -1,3 +1,61 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var cursor = document.querySelector('.cursor');
+  var cursorSize = cursor.offsetWidth;
+
+  document.addEventListener('mousemove', function (e) {
+    var cursorPositionX = e.pageX - (cursorSize / 1);
+    var cursorPositionY = e.pageY - (cursorSize / 1);
+
+    cursor.style.left = cursorPositionX + 'px';
+    cursor.style.top = cursorPositionY + 'px';
+  });
+
+  document.addEventListener('mouseenter', function () {
+    cursor.classList.add('active');
+  });
+
+  document.addEventListener('mouseleave', function () {
+    cursor.classList.remove('active');
+  });
+});
+
+
+
+const paragraphs = document.querySelectorAll('.container p');
+let index = 0;
+
+function typeParagraph() {
+  if (index < paragraphs.length) {
+    const paragraph = paragraphs[index];
+    paragraph.style.display = 'block'; // Exibe o parágrafo
+
+    const text = paragraph.innerHTML;
+    const fullText = text;
+    let currentText = '';
+    let currentIndex = 0;
+
+    const interval = setInterval(() => {
+      if (currentIndex < fullText.length) {
+        currentText += fullText[currentIndex];
+        paragraph.innerHTML = currentText;
+        currentIndex++;
+      } else {
+        clearInterval(interval); // Limpa o intervalo quando o texto estiver totalmente digitado
+        index++; // Passa para o próximo parágrafo
+        typeParagraph(); // Chama a função novamente para o próximo parágrafo
+      }
+    }, 100); // Velocidade de digitação (ajuste conforme necessário)
+  }
+}
+
+// Oculta o conteúdo inicialmente
+paragraphs.forEach((paragraph) => {
+  paragraph.style.display = 'none';
+});
+
+typeParagraph(); // Inicia o efeito de digitação
+
+
 
 
 document.addEventListener('DOMContentLoaded', function() {
